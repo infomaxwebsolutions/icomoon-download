399cbd8bf747faf800fe55dca1015e0e999132af
Florian Müller <mueller@infomax-it.de>
2023-11-23 11:02:12 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,66 @@
+# icomoon-pull
+
+Simple package to pull icomoon font files to the local filesystem. This is useful if you want to use the icomoon 
+fonts in your project without using the icomoon link. 
+
+## Usage
+
+Create .icomoonrc.json in your project root and add the icomoon links you want to download. Then run `npx icomoon-pull`.
+
+```json
+{
+  "first": {
+    "link": "https://i.icomoon.io/public/someThing/Project/style.css",
+    "fontsPath": "path/to/fonts/icomoon/first",
+    "fontsRelativePath": "../../fonts/icomoon/first",
+    "styleFile": "path/to/style/icomoon/first/_init.less",
+    "clean": false 
+  },
+  "second": {
+    "link": "https://i.icomoon.io/public/other/Project/style.css",
+    "fontsPath": "path/to/fonts/icomoon/second",
+    "fontsRelativePath": "../../fonts/icomoon/second",
+    "styleFile": "path/to/style/icomoon/second/_init.less",
+    "clean": false
+  }
+}
+```
+
+
+### Options
+
+#### link
+Type: `String`
+Default value: `''`
+Required: `yes`
+
+A required string to the icomoon development css link. E.g. `https://i.icomoon.io/public/123456/myFoobarProject/style.css`
+
+#### clean
+Type: `Boolean`
+Default value: `false`
+Required: `no`
+
+A true/false value to determine whether to delete the extracted icomoon sources or not. Caution: If you set 
+this to true, the whole fonts folder will be deleted. 
+
+#### fontsPath
+Type: `String`
+Default value: `'.'`
+Required: `no`
+
+Absolute path to your fonts folder where icomoon generated fonts will be copied.
+
+#### fontsRelativePath
+Type: `String`
+Default value: `'fonts'`
+Required: `no`
+
+Relative path to your fonts folder. It should be relative to your icomoon styles file.
+
+#### styleFile
+Type: `String`
+Default value: `'icomoon.css'`
+Required: `no`
+
+Absolute path to your icomoon partial file (including filename). It can be a Sass partial or vanilla CSS file.
---
+++ icomoon-pull.js
@@ -0,0 +1,115 @@
+const download = require('download');
+const path = require('path');
+const url = require('url');
+const fs = require('fs');
+const tmp = require('tmp');
+
+class IcomoonPull {
+  constructor() {
+    fs.readFile('./.icomoonrc.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error('Error reading .icomoonrc.json file. Please create one. See README.md for more information.');
+        return;
+      }
+
+      for (let key in JSON.parse(data)) {
+        try {
+          this.run(key, JSON.parse(data)[key]);
+        } catch (e) {
+          console.error(e);
+        }
+      }
+    })
+  }
+
+  run(key, options) {
+    const defaultOptions = {
+      clean: false,
+      fontsPath: './fonts',
+      fontsRelativePath: 'fonts',
+      styleFile: './icomoon.css',
+    }
+    options = Object.assign(defaultOptions, options);
+
+    console.log('Download icomoon font for ' + key + ' from ' + options.link);
+
+    if (!options.link || options.link.length === 0) {
+      console.error('missing link option.')
+      return;
+    }
+
+    const urlPart = this.urlWithOutFile(options.link);
+    const tmpDirPath = tmp.dirSync({prefix: 'icomoon-pull'}).name;
+
+    download(options.link, tmpDirPath).then(() => {
+      fs.readFile(path.join(tmpDirPath, 'style.css'), (err, data) => {
+        if (err) throw err;
+
+        let fontFiles = this.collectFontFiles(data.toString());
+
+        Promise.all(
+          [...new Set(fontFiles)]
+            .map(x => download(x, tmpDirPath))).then(() => {
+          // create directory if not exists
+          this.createFolderIfNeeded(options.fontsPath, options.clean);
+          this.createFolderIfNeeded(path.dirname(options.styleFile));
+
+          // copy style file and replace font path
+          let style = fs.readFileSync(path.join(tmpDirPath, 'style.css')).toString();
+
+          // write style file
+          let openSyncFD = fs.openSync(options.styleFile, 'w', 0o644);
+          fs.writeSync(openSyncFD, style.replace(new RegExp(urlPart, 'g'), options.fontsRelativePath));
+
+          fontFiles.forEach((file) => this.copy(file, tmpDirPath, options.fontsPath));
+          console.log('Pulling icomoon font for ' + key + ' finished.')
+        });
+      });
+    });
+  }
+
+
+  copy(file, tmpDirPath, fontsPath) {
+    let fileName = url.parse(file).pathname.split("/").pop();
+
+    let tmpFontName = path.join(tmpDirPath, fileName);
+    let destFilePath = path.join(fontsPath, fileName);
+    fs.copyFileSync(tmpFontName, destFilePath);
+  }
+
+  urlWithOutFile(link) {
+    let parsedUrl = url.parse(link);
+    let path = parsedUrl.pathname.split("/");
+    // remove the last part
+    path.pop();
+    // rebuild the url
+    return parsedUrl.protocol + '//' + parsedUrl.hostname + path.join('/');
+  }
+
+  createFolderIfNeeded(dir, clean) {
+    if (clean && fs.existsSync(dir)) {
+      console.log("Clean option was set. Remove existing dir " + dir);
+      fs.rmSync(dir, {recursive: true, force: true});
+    }
+
+    if (!fs.existsSync(dir)) {
+      console.log("Create directory " + dir);
+      fs.mkdirSync(dir, {recursive: true});
+    }
+  }
+
+  collectFontFiles(style) {
+    let regex = new RegExp(/url\(\'(.*)\?.*\'\)/g);
+    let fontFiles = [];
+    let match = '';
+    while (match = regex.exec(style)) {
+      if (match.index === regex.lastIndex) {
+        regex.lastIndex++;
+      }
+      fontFiles.push(match[1]);
+    }
+    return fontFiles;
+  }
+}
+
+new IcomoonPull();
